547af00fb7c23dd7b71f031493c722bc27a4cf2e
Florian Torres <torres.florian1@gmail.com>
2018-02-19 10:32:59 +0100

    début td3

--- td3/ex2/minimal.c
+++ td3/ex2/minimal.c
@@ -50,10 +50,10 @@ void drawLandmark() {
 	// Axe des abscisses rouge
 	glColor3ub(255, 0, 0);
 	glBegin(GL_LINES);
-		glVertex2f(-4, 0);
-		glVertex2f( 4, 0);
+		glVertex2f(-8, 0);
+		glVertex2f( 8, 0);
 	glEnd();
-	for (i = -4; i < 4; ++i)
+	for (i = -8; i < 8; ++i)
 	{
 		glBegin(GL_LINES);
 			glVertex2f(i, -.05);
@@ -64,10 +64,10 @@ void drawLandmark() {
 	// Axe des ordonnées vert
 	glColor3ub(0, 255, 0);
 	glBegin(GL_LINES);
-		glVertex2f(0, -3);
-		glVertex2f(0,  3);
+		glVertex2f(0, -6);
+		glVertex2f(0,  6);
 	glEnd();
-	for (i = -3; i < 3; ++i)
+	for (i = -6; i < 6; ++i)
 	{
 		glBegin(GL_LINES);
 			glVertex2f(-.05, i);
@@ -88,7 +88,6 @@ void drawSquare(int full) {
 	glEnd();
 }
 
-/* Fonctions dessins objets canoniques */
 void drawCircle(int full) {
 	int nbPoints = 64;
 	int i;
@@ -104,33 +103,112 @@ void drawCircle(int full) {
 	glEnd();
 }
 
-int random_0_1(void){
-    srand(time(NULL));
-    return (rand()&1);
+void drawRoundedSquare() {
+	float radius = .5;
+	glPushMatrix();
+		glPushMatrix();
+			glTranslatef(-.5, -.5, 0);
+			glScalef(1-radius, 1-radius, 1);
+			glTranslatef(.5, .5, 0);
+			drawCircle(1);
+		glPopMatrix();
+		glPushMatrix();
+			glTranslatef(-.5, .5, 0);
+			glScalef(1-radius, 1-radius, 1);
+			glTranslatef(.5, -.5, 0);
+			drawCircle(1);
+		glPopMatrix();
+		glPushMatrix();
+			glTranslatef(.5, -.5, 0);
+			glScalef(1-radius, 1-radius, 1);
+			glTranslatef(-.5, .5, 0);
+			drawCircle(1);
+		glPopMatrix();
+		glPushMatrix();
+			glTranslatef(.5, .5, 0);
+			glScalef(1-radius, 1-radius, 1);
+			glTranslatef(-.5, -.5, 0);
+			drawCircle(1);
+		glPopMatrix();
+		glPushMatrix();
+			glScalef(radius, 1, 1);
+			drawSquare(1);
+		glPopMatrix();
+		glPushMatrix();
+			glScalef(1, radius, 1);
+			drawSquare(1);
+		glPopMatrix();
+	glPopMatrix();
+}
+
+GLuint createFirstArmIDList() {
+	glPushMatrix();
+		glScalef(4, 4, 1);
+		drawCircle(1);
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(6, 0, 0);
+		glScalef(2, 2, 1);
+		drawCircle(1);
+	glPopMatrix();
+
+	glBegin(GL_QUADS);
+		glVertex2f(0, 2);
+		glVertex2f(0, -2);
+		glVertex2f(6, -1);
+		glVertex2f(6, 1);
+	glEnd();
+	return glGenLists(1);
+}
+
+GLuint createSecondArmIDList() {
+	glColor3ub(180, 180, 180);
+	glPushMatrix();
+		drawRoundedSquare();
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(4, 0, 0);
+		drawRoundedSquare();
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(2, 0, 0);
+		glScalef(4.6, .6, 1);
+		drawSquare(1);
+	glPopMatrix();
+	return glGenLists(2);
+}
+
+GLuint createThirdArmIDList() {
+	glColor3ub(255, 180, 100);
+	glPushMatrix();
+		glScalef(.6, .6, 1);
+		drawSquare(1);
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(1.5, 0, 0);
+		glScalef(3, .4, 1);
+		drawSquare(1);
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(3, 0, 0);
+		glScalef(.8, .8, 1);
+		drawCircle(1);
+	glPopMatrix();
+	return glGenLists(3);
 }
 
 void resize(); //Redimensionne le viewport et rafraichit le repère
-void displayPalette(); //Affiche les couleurs de la palette
-void selectColor(int x, int *r, int *g, int *b); // Modifie la couleur actuelle en fonction du clic
-
 
 int main(int argc, char** argv) {
-	int mode; //0: Mode dessin, 1: Mode palette
-	int angle; //rotation du cercle jaune en degrés
 	float x, y; // Coordonnées du curseur au clic
 	int r, g, b; // Valeurs rgb de la couleur courante
 	float point_size; //Largeur du point
-	int right_click;
-	float rand_x, rand_y;
 
 	PrimitiveList primitives = NULL;
-	mode = 0;
-	right_click = 0;
-	angle = 0;
 	r = g = b = 255; //Couleur blanche
 	x = y = 0;
-	rand_x = rand_y = 0;
-	point_size = 2.0f;
+	point_size = 1.0f;
 
 	addPrimitive(allocPrimitive(GL_LINE_STRIP), &primitives);
 
@@ -148,6 +226,8 @@ int main(int argc, char** argv) {
 
 	resize();
 
+
+	glScalef(.25, .25, 1);
 	glColor3ub(r, g, b); //Initialise la couleur à blanc
 	glLineWidth(point_size); 
 	glPointSize(point_size); 	
@@ -158,6 +238,10 @@ int main(int argc, char** argv) {
 
 	/* Boucle d'affichage */
 	int loop = 1;
+	int alpha, beta, gamma;
+	alpha = 45;
+	beta = -10;
+	gamma = 35;
 	while(loop) {
 
 		/* Récupération du temps au début de la boucle */
@@ -165,55 +249,28 @@ int main(int argc, char** argv) {
 		
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		if (mode == 0) {
-			drawLandmark(); // Dessin du repère
+		drawLandmark(); // Dessin du repère
+		glColor3ub(255, 255, 255);
+		glPushMatrix();
+			glRotatef(alpha, 0, 0, 1);
+			drawFirstArm();
 
-			//Dessin du cercle orange
-			glColor3ub(255, 150, 0);
-			glTranslatef(1,2,0);
-			drawCircle(1); 
-			glTranslatef(-1,-2,0);
+			glTranslatef(6, 0, 0);
+			glRotatef(beta, 0, 0, 1);
+			drawSecondArm();
 
-			//Dessin du carré rouge
-			glColor3ub(255, 0, 0);
-			glRotatef(45, 0.0, 0.0, 1.0);
-			glTranslatef(2,0,0);
-			drawSquare(1);
-			glTranslatef(-2,0,0);
-			glRotatef(-45, 0.0, 0.0, 1.0);
+			glTranslatef(4, 0, 0);
+			glRotatef(gamma, 0, 0, 1);
+			drawThirdArm();
+			glRotatef(gamma*2, 0, 0, 1);
+			drawThirdArm();
+			glRotatef(gamma/2, 0, 0, 1);
+			drawThirdArm();
+		glPopMatrix();
 
-			//Dessin du carré violet
-			glColor3ub(255, 0, 255);
-			glTranslatef(2,0,0);
-			glRotatef(45, 0.0, 0.0, 1.0);
-			drawSquare(1);
-			glRotatef(-45, 0.0, 0.0, 1.0);
-			glTranslatef(-2,0,0);
-
-			//Dessin du carré jaune
-			glColor3ub(255, 255, 0);
-			glTranslatef(x,y,0);
-			glRotatef(angle, 0.0, 0.0, 1.0);
-			drawSquare(1);
-			glRotatef(-angle, 0.0, 0.0, 1.0);
-			glTranslatef(-x,-y,0);
-
-			//Dessin du cercle bleu
-			rand_x = rand()%2 ? rand_x + .1 : rand_x - .1;
-			rand_y = rand()%2 ? rand_y + .1 : rand_y - .1;
-			if (rand_x < -4) rand_x = -4;
-			if (rand_x > 4) rand_x = 4;
-			if (rand_y < -3) rand_y = -3;
-			if (rand_y > 3) rand_y = 3;
-			glColor3ub(0, 0, 255);
-			glTranslatef(rand_x, rand_y, 0);
-			drawCircle(1);
-			glTranslatef(-rand_x, -rand_y, 0);
-
-			// drawPrimitives(primitives); //On dessine les primitives
-		} else {
-			displayPalette(); //On affiche les couleurs pour la sélection
-		}
+		alpha++;
+		beta++;
+		gamma++;
 
 		/* Echange du front et du back buffer : mise à jour de la fenêtre */
 		SDL_GL_SwapBuffers();
@@ -231,72 +288,21 @@ int main(int argc, char** argv) {
 			/* Traitement d'evenements  */
 			switch(e.type) {
 
-				case SDL_MOUSEMOTION:
-					if (right_click)
-					{
-						angle = 360 * e.button.x / WINDOW_WIDTH;
-					}
-					break;
-
-				/* Clic souris */
-				case SDL_MOUSEBUTTONDOWN:
-					switch (mode) {
-						case 0:	//Mode dessin : Ajout d'un point
-							if (e.button.button == SDL_BUTTON_RIGHT)
-							{
-								right_click = 1;
-								primitives->primitiveType = GL_LINE_LOOP;
-							} else {
-								x = -4 + 8. * e.button.x / WINDOW_WIDTH;
-								y = -(-3 + 6. * e.button.y / WINDOW_HEIGHT);
-								printf("x, y = %f, %f\n", x, y);
-								addPointToList(allocPoint(x, y, r, g, b), &primitives->points);
-							}
-							break;
-						case 1: //Mode palette : Sélection de couleur 
-							selectColor(e.button.x, &r, &g, &b);
-							break;
-						default:
-							break;
-					}
-					break;
-
-				/* Clic souris */
-				case SDL_MOUSEBUTTONUP:
-					if (e.button.button == SDL_BUTTON_RIGHT)
-					{
-						right_click = 0;
-					}
-					break;
-
 				/* Touche clavier appuyée */
 				case SDL_KEYDOWN:
 					switch (e.key.keysym.sym) {
-						case SDLK_SPACE:
-							mode = 1;
-							break;
-						case SDLK_p:
-							addPrimitive(allocPrimitive(GL_POINTS), &primitives);
-							break;
-						case SDLK_l:
-							addPrimitive(allocPrimitive(GL_LINES), &primitives);
-							break;
-						case SDLK_t:
-							addPrimitive(allocPrimitive(GL_TRIANGLES), &primitives);
-							break;
 						case SDLK_q:
 							loop = 0;
 							break;
 						default:
 							break;
 					}
-					break;
+				break;
 
 				/* Touche clavier relachée */
 				case SDL_KEYUP:
 					switch (e.key.keysym.sym) {
 						case SDLK_SPACE:
-							mode = 0;
 							glClear(GL_COLOR_BUFFER_BIT);
 						default:
 							break;
@@ -437,29 +443,4 @@ void resize() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(-4., 4., -3., 3.);
-}
-
-void displayPalette() {
-	int nb_colors, i;
-	float color_width;
-	nb_colors = 8;
-	color_width = 2 * WINDOW_WIDTH / (float)nb_colors * 100 / WINDOW_WIDTH / 100 ;
-	glBegin(GL_QUADS);
-		for(i = 0; i < nb_colors; ++i) {
-			glColor3ub((255 / nb_colors) * i, 0, 255);
-			glVertex2f(-1 + i * color_width, -1);
-			glVertex2f(-1 + (i + 1) * color_width, -1);
-			glVertex2f(-1 + (i + 1) * color_width, 1);
-			glVertex2f(-1 + i * color_width, 1);
-		}
-	glEnd();
-}
-
-void selectColor(int x, int *r, int *g, int *b) {
-	int nb_colors, num_bande;
-	nb_colors = 8;
-	num_bande = x / (WINDOW_WIDTH / nb_colors);
-	*r = (255 / nb_colors) * num_bande;
-	*g = 0;
-	*b = 255;
 }
